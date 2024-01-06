5eca8c62d3b105eaada210b4223cade736673de0
Muhammad-Saad-Usmani7 <Saadrockerz50@gmail.com>
2024-01-06 15:33:25 +0500

    sssss

--- server.mjs
+++ server.mjs
@@ -13,31 +13,36 @@ let userData = [];
 let flag = false;
 // console.o
 app.post('/user123', (req, res) => {
-    const body = req.body;
-  
-    if (!body.fullName || !body.email || !body.password) {
-      return res.status(400).send({
-        message: "fullname, email, and password are required!",
+  const body = req.body;
+  if (!body.fullName) {
+    res.status(404).send({
+      message: "fullname required!",
+    });
+    if (!body.email) {
+      res.status(404).send({
+        email: "email required!",
       });
+      if (!body.password) {
+        res.status(404).send({
+          password: "password required!",
+        });
+      } else {
+        flag = true;
+      }
+    } else {
+      flag = true;
     }
-  
+  } else {
+    flag = true;
+  }
+  if(flag){
     const obj1 = {
-      fullName: body.fullName,
-      email: body.email,
-      password: body.password
-    };
-  
+        fullName : body.fullName,
+        email : body.email,
+        password : body.password
+    }
     userData.push(obj1);
-    console.log(userData);
-  
-    // Adding CORS headers
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'OPTIONS, POST, GET');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-  
-    res.status(201).send('user is created');
-  });
-  
+  }
   
   console.log(userData);
   res.status(201).send('user is created');
