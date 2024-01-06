c001aa2a807daed52084df9282588b4b8c2ab031
Muhammad-Saad-Usmani7 <Saadrockerz50@gmail.com>
2024-01-06 15:25:06 +0500

    working..

--- server.mjs
+++ server.mjs
@@ -3,7 +3,7 @@
 import express from "express";
 import cors from "cors";
 const app = express();
-app.use(cors());
+app.use(cors({origin : "*"}));
 
 app.use(express.json());
 const port = process.env.PORT || 3000;
