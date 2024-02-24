021f9575fe4c58da2636794a93e5c9f50c5f077b
Muhammad-Saad-Usmani7 <Saadrockerz50@gmail.com>
2024-02-25 00:02:09 +0500

    dooing...

--- server.mjs
+++ server.mjs
@@ -4,7 +4,10 @@ import cors from "cors";
 const port = process.env.PORT || 3000;
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: 'POST'
+}));
 app.use(express.json());
 
 let userData = [];
